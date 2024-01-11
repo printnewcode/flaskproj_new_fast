e1d0d17bd1abe9993ef9718ac6b00531143c8a58
printnewcode <zenit200803@mail.ru>
2024-01-11 16:22:59 +0300

    a

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import flask
+from typing import Union
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    return '<h1>Hello, world!</h1>'
+
+
+@app.route('/user/<username>/<age>')
+def user_info(username: str, age: Union[int, str]) -> str:
+    info = (
+        f'<h1>Hi, {username}!</h1><br>'
+        f' <p>You are {age} years old.</p>'
+    )
+    return info
+
+
+app.run(debug=True)
