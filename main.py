5d526df87941ef644f90344df9286ede1ab8c8df
printnewcode <zenit200803@mail.ru>
2024-01-18 15:38:06 +0300

    v1.0

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from typing import Union
+import time
+import random
 
-from flask import Flask
+from flask import Flask, render_template
+
+from typing import Union
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index() -> str:
-    return '<h1>Hello, world!</h1>'
+    return render_template('index.html', name='Zaxar')
+
+
+@app.route('/randomizer/<num_1>/<num_2>')
+def randomizer(num_1, num_2) -> Union[str, int]:
+    return render_template('random.html', numRandom=random.randint(int(num_1), int(num_2)))
+
+
+@app.route('/time/')
+def timer():
+    seconds = time.time()
+    local_time = time.ctime(seconds)
+    return render_template('time.html', timeNow=local_time)
 
 
 @app.route('/user/<username>/<age>')
